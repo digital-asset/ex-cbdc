3a0f2080e8f5f3beb257d00a7d523f9196797eac
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2021-05-07 14:04:26 +0200

    fix headers

--- daml.mk
+++ daml.mk
@@ -1,3 +1,8 @@
+#
+# Copyright (c) 2021, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
 comma := ,
 daml ?= daml
 rdars := $(shell scripts/getDamlProjects.sh)
--- scripts/getDamlProjects.sh
+++ scripts/getDamlProjects.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env bash
+#
+# Copyright (c) 2021, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
 set -euo pipefail
 
 yamls=( $(find . -mindepth 2 -name 'daml.yaml') )
